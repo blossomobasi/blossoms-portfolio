cc3a359e0b59d118410f5dbb2d2bc204a62bef09
Obasi Chizitere <blossomobasi2@gmail.com>
2024-02-13 10:05:26 +0100

    Add project data in a data folder

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,36 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
 import Button from "./Button";
+import { projects as projectApi } from "../data/projects";
 
 const Projects = () => {
-  const projects = [
-    {
-      href: "https://chow-frontend-2m7o54od6-herdeybayor.vercel.app/",
-      src: "/images/chow-project.png",
-      alt: "Chow Project",
-      title: "CHOW - where speed meets satisfaction",
-      development: true,
-      content:
-        "Chow is a website that empowers users to set the pace in delivering food, ensuring that every order reaches its destination with unparalleled speed and efficiency.",
-    },
-    {
-      href: "https://the-wild-oasis-dev-b.vercel.app/",
-      src: "/images/the-wild-oasis.png",
-      alt: "The wild oasis Project",
-      title: "The wild Oasis",
-      content:
-        "The Wild Oasis simplifies cabin rentals, making your getaway dreams a reality. Find your perfect retreat and book your wilderness escape effortlessly. Your adventure starts here.",
-      type: "reverse",
-    },
-    {
-      href: "https://tridehq-clone.vercel.app/",
-      src: "/images/tridehq-clone.png",
-      alt: "Tridehq clone Project",
-      title: "Tridehq-clone - Banking made easy for churches and ministries",
-      content:
-        "Tride helps you receive funds faster from your members and donors with dedicated bank accounts and online payment options.",
-    },
-  ];
+  const projects = projectApi;
+
   return (
     <section id="projects">
       <div className="uppercase font-extrabold text-stone-600 relative w-fit mb-10 text-xl italic -z-10">
---
+++ src/data/projects.ts
@@ -0,0 +1,28 @@
+export const projects = [
+  {
+    href: "https://chow-frontend-2m7o54od6-herdeybayor.vercel.app/",
+    src: "/images/chow-project.png",
+    alt: "Chow Project",
+    title: "CHOW - where speed meets satisfaction",
+    development: true,
+    content:
+      "Chow is a website that empowers users to set the pace in delivering food, ensuring that every order reaches its destination with unparalleled speed and efficiency.",
+  },
+  {
+    href: "https://the-wild-oasis-dev-b.vercel.app/",
+    src: "/images/the-wild-oasis.png",
+    alt: "The wild oasis Project",
+    title: "The wild Oasis",
+    content:
+      "The Wild Oasis simplifies cabin rentals, making your getaway dreams a reality. Find your perfect retreat and book your wilderness escape effortlessly. Your adventure starts here.",
+    type: "reverse",
+  },
+  {
+    href: "https://tridehq-clone.vercel.app/",
+    src: "/images/tridehq-clone.png",
+    alt: "Tridehq clone Project",
+    title: "Tridehq-clone - Banking made easy for churches and ministries",
+    content:
+      "Tride helps you receive funds faster from your members and donors with dedicated bank accounts and online payment options.",
+  },
+];
