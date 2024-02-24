626d1cee521a4d50bd5a5dfd1d09adefd86a2fe1
Obasi Chizitere <blossomobasi2@gmail.com>
2024-02-24 14:49:29 +0100

    update mobile design for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,9 +6,7 @@ import Icon from "./Icon";
 const Footer = () => {
   return (
     <footer className="border-t-2 border-t-stone-100 md:pt-10 pt-5 mb-5 flex flex-col sm:items-center">
-      <h2 className="font-semibold text-xl text-stone-600">
-        Obasi Chizitere Blossom
-      </h2>
+      <h2 className="font-semibold text-xl text-stone-600">Blossom Obasi</h2>
 
       <div className="flex gap-5 mt-5">
         <Icon url="https://www.instagram.com/iambl0ss0m/" title="Instagram">
