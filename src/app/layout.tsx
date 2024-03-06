4cd8adb41d143d91f260063392f087530ddd84a4
Obasi Chizitere <blossomobasi2@gmail.com>
2024-03-06 15:27:15 +0100

    Change font family to Exo_2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
 import type { Metadata } from "next";
-import { Montserrat_Alternates } from "next/font/google";
-// import { Exo_2 } from "next/font/google";
+// import { Montserrat_Alternates } from "next/font/google";
+import { Exo_2 } from "next/font/google";
 
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 
-const montserrat_alternates = Montserrat_Alternates({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
-});
-
-// const exo_2 = Exo_2({
+// const montserrat_alternates = Montserrat_Alternates({
 //   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 //   subsets: ["latin"],
 // });
+
+const exo_2 = Exo_2({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Blossom Obasi",
@@ -27,11 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={
-          montserrat_alternates.className + " flex flex-col items-center"
-        }
-      >
+      <body className={exo_2.className + " flex flex-col items-center"}>
         <NavBar />
         <div className="max-w-[100rem]">{children}</div>
       </body>
