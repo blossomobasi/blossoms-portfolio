6a0219fc9aa21323bc2611f48fb1ca2e752ce1b8
Obasi Chizitere <blossomobasi2@gmail.com>
2024-03-06 15:03:09 +0100

    uninstall framer motion

--- src/components/ScrollUpButton.tsx
+++ src/components/ScrollUpButton.tsx
@@ -1,19 +1,13 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 import { BsArrowUp } from "react-icons/bs";
 
 const ScrollUpButton = () => {
-  // Fixed value
-  const [visibility, setVisibility] = useState(true);
   const router = useRouter();
-
   return (
     <button
-      className={`${
-        visibility ? "block" : "hidden"
-      } bg-stone-500 text-white p-2 rounded-sm absolute bottom-5 right-5`}
+      className="bg-stone-500 text-white p-2 rounded-sm absolute bottom-5 right-5"
       onClick={() => router.push("#home")}
     >
       <BsArrowUp />
