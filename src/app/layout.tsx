e188b3d323222305f97be168b47dbb2f2a5d8c1a
Obasi Chizitere <blossomobasi2@gmail.com>
2024-04-25 07:36:33 +0100

    Modified: meta title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,12 @@ import { Exo_2 } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 
-// const montserrat_alternates = Montserrat_Alternates({
-//   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-//   subsets: ["latin"],
-// });
+/*
+const montserrat_alternates = Montserrat_Alternates({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+*/
 
 const exo_2 = Exo_2({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -16,7 +18,7 @@ const exo_2 = Exo_2({
 });
 
 export const metadata: Metadata = {
-  title: "Blossom Obasi",
+  title: "Blossom Obasi | Frontend Developer",
   description: "About Blossom Obasi",
 };
 
