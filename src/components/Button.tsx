490f346050bf4b83ca0d1558916c0b9eac0e2b85
Obasi Chizitere <blossomobasi2@gmail.com>
2024-04-29 16:15:00 +0100

    Edit: Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,6 +13,8 @@ interface IButtonProps
 const Button = ({ children, url }: IButtonProps) => {
   const className =
     "bg-stone-500 hover:bg-stone-600 text-white h-9 px-4 rounded-md";
+  const name = "Blossom";
+  console.log(name);
 
   if (url)
     return (
