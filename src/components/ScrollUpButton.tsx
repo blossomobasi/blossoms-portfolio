7b6775fab0f96c2854f016a693471b2aed0f14f9
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-02 07:09:39 +0100

    Modified ScrollUpButton

--- src/components/ScrollUpButton.tsx
+++ src/components/ScrollUpButton.tsx
@@ -1,14 +1,31 @@
+// "use client";
+
+// import { useRouter } from "next/navigation";
+// import { BsArrowUp } from "react-icons/bs";
+
+// const ScrollUpButton = () => {
+//   const router = useRouter();
+//   return (
+//     <button
+//       className="bg-stone-500 text-white p-2 rounded-sm absolute bottom-5 right-5"
+//       onClick={() => router.push("#home")}
+//     >
+//       <BsArrowUp />
+//     </button>
+//   );
+// };
+
+// export default ScrollUpButton;
+
 "use client";
 
-import { useRouter } from "next/navigation";
 import { BsArrowUp } from "react-icons/bs";
 
 const ScrollUpButton = () => {
-  const router = useRouter();
   return (
     <button
       className="bg-stone-500 text-white p-2 rounded-sm absolute bottom-5 right-5"
-      onClick={() => router.push("#home")}
+      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
     >
       <BsArrowUp />
     </button>
