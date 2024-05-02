8ae29db850d635c8ce47208d291897f1419432c6
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-02 07:15:12 +0100

    Comment

--- src/components/ScrollUpButton.tsx
+++ src/components/ScrollUpButton.tsx
@@ -1,31 +1,14 @@
-// "use client";
-
-// import { useRouter } from "next/navigation";
-// import { BsArrowUp } from "react-icons/bs";
-
-// const ScrollUpButton = () => {
-//   const router = useRouter();
-//   return (
-//     <button
-//       className="bg-stone-500 text-white p-2 rounded-sm absolute bottom-5 right-5"
-//       onClick={() => router.push("#home")}
-//     >
-//       <BsArrowUp />
-//     </button>
-//   );
-// };
-
-// export default ScrollUpButton;
-
 "use client";
 
+import { useRouter } from "next/navigation";
 import { BsArrowUp } from "react-icons/bs";
 
 const ScrollUpButton = () => {
+  const router = useRouter();
   return (
     <button
       className="bg-stone-500 text-white p-2 rounded-sm absolute bottom-5 right-5"
-      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      onClick={() => router.push("#home")}
     >
       <BsArrowUp />
     </button>
@@ -33,3 +16,20 @@ const ScrollUpButton = () => {
 };
 
 export default ScrollUpButton;
+
+// "use client";
+
+// import { BsArrowUp } from "react-icons/bs";
+
+// const ScrollUpButton = () => {
+//   return (
+//     <button
+//       className="bg-stone-500 text-white p-2 rounded-sm absolute bottom-5 right-5"
+//       onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+//     >
+//       <BsArrowUp />
+//     </button>
+//   );
+// };
+
+// export default ScrollUpButton;
