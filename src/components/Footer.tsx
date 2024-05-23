88349fe3d48ffcddcb6ea2d2a774470b6531e6a4
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-23 12:13:44 +0100

    feature: darkmode for footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import Icon from "./Icon";
 
 const Footer = () => {
   return (
-    <footer className="border-t-2 border-t-stone-100 md:pt-10 pt-5 mb-5 flex flex-col items-center">
+    <footer className="border-t-2 border-t-stone-100 dark:border-t-stone-900 md:pt-10 pt-5 mb-5 flex flex-col items-center">
       <div className="flex gap-5">
         <Icon url="https://www.instagram.com/iambl0ss0m/" title="Instagram">
           <BsInstagram />
@@ -21,7 +21,7 @@ const Footer = () => {
         </Icon>
       </div>
 
-      <p className="mt-3 text-sm text-stone-600">
+      <p className="mt-3 text-sm text-stone-600 dark:text-stone-400">
         Copyright &copy; 2024. All rights are reserved |
         <strong> Blossom Obasi</strong>
       </p>
