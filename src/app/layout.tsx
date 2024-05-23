2bf6f5264a39923939afff52e711b74ba96b8108
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-23 12:41:29 +0100

    Fix scroll to top button to be compatable with all browsers, change font and delete scrollToTop package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,19 @@
 import type { Metadata } from "next";
-import { Exo_2 } from "next/font/google";
+// import { Exo_2 } from "next/font/google";
 import "./globals.css";
 import { DarkModeProvider } from "@/context/DarkModeContext";
 
-/* import { Montserrat_Alternates } from "next/font/google";
+import { Montserrat_Alternates } from "next/font/google";
 
 const montserrat_alternates = Montserrat_Alternates({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
-*/
 
-const exo_2 = Exo_2({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
-});
+// const exo_2 = Exo_2({
+//   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Blossom Obasi | Frontend Developer",
@@ -30,7 +29,7 @@ export default function RootLayout({
     <DarkModeProvider>
       <html lang="en">
         <body
-          className={`${exo_2.className} max-w-[100rem] dark:bg-black dark:text-stone-200`}
+          className={`${montserrat_alternates.className} max-w-[100rem] dark:bg-black dark:text-stone-200`}
         >
           {children}
         </body>
--- src/components/ScrollUpButton.tsx
+++ src/components/ScrollUpButton.tsx
@@ -1,16 +1,35 @@
 "use client";
 
-import ScrollToTop from "react-scroll-up";
 import { BsArrowUp } from "react-icons/bs";
+import { useState, useEffect, use } from "react";
+
+import React from "react";
 
 const ScrollUpButton = () => {
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    window.onscroll = () => {
+      if (window.scrollY > 0) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
+    };
+  }, []);
+
   return (
-    <div className="z-50">
-      <ScrollToTop showUnder={160}>
-        <button className="bg-stone-500 z-50 text-white p-2 rounded-sm">
-          <BsArrowUp />
-        </button>
-      </ScrollToTop>
+    <div>
+      <button
+        onClick={() => {
+          window.scrollTo({ top: 0, behavior: "smooth" });
+        }}
+        className={`fixed bottom-5 right-5 bg-stone-500 dark:bg-stone-800 z-50 text-white p-2 rounded-sm ${
+          scroll ? "block" : "hidden"
+        }`}
+      >
+        <BsArrowUp />
+      </button>
     </div>
   );
 };
