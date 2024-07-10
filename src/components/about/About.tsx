699780c93eaae1acc51849ad222d632373721ac0
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-10 11:30:04 +0100

    Use TextHeader component to render header text

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,14 +1,12 @@
 import AboutSlice1 from "./AboutSlice1";
 import AboutSlice2 from "./AboutSlice2";
 import AboutSlice3 from "./AboutSlice3";
+import TextHeader from "../TextHeader";
 
 const About = () => {
     return (
         <section className="md:pt-28" id="about">
-            <div className="uppercase font-extrabold text-stone-600 dark:text-stone-300 relative w-fit mb-10 text-xl italic -z-10">
-                About me
-                <div className="absolute h-0.5 w-full bg-stone-600 dark:bg-stone-300" />
-            </div>
+            <TextHeader>About Me</TextHeader>
 
             <AboutSlice1 />
             <AboutSlice2 />
