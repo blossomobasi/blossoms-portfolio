5e6c8d26bc2afa71528d23ead3ba8c77743b53fd
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-10 11:30:46 +0100

    Use dynamic date insead of static text date

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,14 @@
 import SocialIcons from "./SocialIcons";
 
 const Footer = () => {
+    const year = new Date().getFullYear();
+
     return (
         <footer className="border-t-2 border-t-stone-100 dark:border-t-stone-900 md:pt-10 pt-5 mb-5 flex flex-col items-center">
             <SocialIcons />
 
             <p className="mt-3 text-sm text-stone-600 dark:text-stone-400">
-                Copyright &copy; 2024. All rights are reserved |<strong> Blossom Obasi</strong>
+                Copyright &copy; {year}. All rights are reserved |<strong> Blossom Obasi</strong>
             </p>
         </footer>
     );
