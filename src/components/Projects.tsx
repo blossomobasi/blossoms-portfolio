4c1049bff5491f848887044ccd58ba3f9450e9db
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-10 11:29:55 +0100

    Use TextHeader component to render header text

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,16 +6,14 @@ import { FaGithub } from "react-icons/fa6";
 
 import Icon from "./Icon";
 import Button from "./Button";
+import TextHeader from "./TextHeader";
 
 const Projects = () => {
     const projects = projectApi;
 
     return (
         <section id="projects">
-            <div className="uppercase font-extrabold text-stone-600 dark:text-stone-300 relative w-fit mb-10 text-xl italic -z-10">
-                Projects
-                <div className="absolute h-0.5 w-full bg-stone-600 dark:bg-stone-300" />
-            </div>
+            <TextHeader>Projects</TextHeader>
 
             <div className="space-y-10 md:space-y-10">
                 {projects.map((project) => (
