d92027950d4202bd26c07567ef5681a88be5f4c5
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-16 13:40:41 +0100

    Update next button

--- src/components/NextButton.tsx
+++ src/components/NextButton.tsx
@@ -24,14 +24,14 @@ const NextButton = () => {
     return (
         <Link
             href={nextPathname.toLocaleLowerCase()}
-            className={`fixed top-[calc(100%-8rem)] right-10 group ${
+            className={`fixed top-[calc(100%-8rem)] right-10 group border dark:border-stone-800 border-stone-400 hover:border-stone-500 py-1 px-2 rounded-md ${
                 pathname === "/contact" && "hidden"
             }`}
         >
-            <span className="text-sm opacity-50 group-hover:opacity-100 transition-all duration-300">
+            <span className="text-sm opacity-50 dark:group-hover:opacity-100 transition-all duration-300">
                 Next
             </span>
-            <p className="text-lg font-medium flex items-center">
+            <p className="text-lg font-medium flex items-center dark:text-white text-stone-600">
                 {nextPathname}
                 <span className="group-hover:translate-x-2 transition-all duration-300">
                     <FaAngleRight />
