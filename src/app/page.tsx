5d8a2ae86fa66d30cf1cdebe396d917b84c93e6b
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-16 13:13:35 +0100

    Change to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,55 @@
-import About from "@/components/about/About";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
-import Home from "@/components/Home";
-import NavBar from "@/components/NavBar";
-import Projects from "@/components/Projects";
-import ScrollUpButton from "@/components/ScrollUpButton";
+"use client";
 
-function Page() {
+import SocialIcons from "@/components/SocialIcons";
+import TechStack from "@/components/TechStack";
+import Image from "next/image";
+import { Typewriter } from "react-simple-typewriter";
+
+const HomePage = () => {
     return (
-        <main className="w-full">
-            <NavBar />
+        <section className="p-2 md:mb-0 mb-20 max-w-[110rem] mx-auto">
+            <div className="flex items-center md:pt-10 gap-x-10 gap-y-5 flex-col-reverse lg:flex-row">
+                <div className="text-sm w-full text-center md:text-left">
+                    <div className="mb-5 text-lg font-medium italic">
+                        <Typewriter
+                            words={["Hey There, I'm Blossom 👋🏻", "Lovely meeting you😊"]}
+                            cursor={true}
+                            cursorStyle="_"
+                            typeSpeed={100}
+                            deleteSpeed={100}
+                            loop={true}
+                        />
+                    </div>
+                    <h1 className="lg:text-6xl md:text-5xl text-3xl font-extrabold">
+                        Full-stack Developer
+                    </h1>
+                    <p className="mt-3 text-stone-700 dark:text-stone-500">
+                        Hi! I&apos;m{" "}
+                        <span className="font-semibold text-base text-black dark:text-stone-300">
+                            Blossom Obasi
+                        </span>
+                        , I specialize in building dynamic, flexible, and highly functional web
+                        applications designed for a seamless user experience.
+                    </p>
 
-            <section className="lg:px-40 md:px-20 px-5 pt-16">
-                <Home />
-                <About />
-                <Projects />
-                <Contact />
-                <Footer />
-            </section>
+                    {/* Social media links */}
+                    <SocialIcons />
+                </div>
 
-            <ScrollUpButton />
-        </main>
+                <div className="border-[1rem] border-stone-300 dark:border-stone-700 rounded-full md:w-[55%] h-full -z-10">
+                    <Image
+                        src="/images/blossom.jpg"
+                        alt="Blossom"
+                        width={500}
+                        height={500}
+                        priority
+                        className="md:h-full sm:h-[23rem] w-full rounded-full contrast-125 aspect-square md:aspect-auto object-cover scale-95"
+                    />
+                </div>
+            </div>
+            <TechStack />
+        </section>
     );
-}
+};
 
-export default Page;
+export default HomePage;
