7ad3a9d85266350787639ae9663274ef25f852cc
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-16 13:14:36 +0100

    update margin

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     const year = new Date().getFullYear();
 
     return (
-        <footer className="border-t-2 border-t-stone-100 dark:border-t-stone-900 md:pt-10 pt-5 mb-5 flex flex-col items-center">
+        <footer className="border-t-2 border-t-stone-100 dark:border-t-stone-900 mb-5 flex flex-col items-center mt-10">
             <SocialIcons />
 
             <p className="mt-3 text-sm text-stone-600 dark:text-stone-400">
