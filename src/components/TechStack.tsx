02fca8019b4aa5e6ef4b097a17bf354b111e5e8d
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-19 11:22:08 +0100

    Render tools

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,11 +1,11 @@
-import { frontendStacks as frontendStacksApi } from "@/data/techStack";
-import { backendStacks as backendStacksApi } from "@/data/techStack";
+import { frontendStacks as frontendStacksApi, tools as toolsApi, backendStacks as backendStacksApi } from "@/data/techStack";
 import Image from "next/image";
 import TextHeader from "./TextHeader";
 
 const TechStack = () => {
     const frontendStacks = frontendStacksApi;
     const backendStacks = backendStacksApi;
+    const tools = toolsApi;
 
     return (
         <div className="mt-10">
@@ -42,6 +42,30 @@ const TechStack = () => {
 
                 <div className="flex space-x-10 flex-wrap items-center flex-shrink-0">
                     {backendStacks.map((stack) => (
+                        <span key={stack.alt}>
+                            <Image
+                                className={`cursor-pointer transition-all duration-300 hover:border dark:border-[#0a0a0a] dark:hover:border-white p-2 border-stone-300 rounded-md ${
+                                    stack.change &&
+                                    "dark:filter dark:grayscale dark:bg-red-300 dark:scale-75"
+                                }`}
+                                title={stack.alt}
+                                src={stack.src}
+                                alt={stack.alt}
+                                height={60}
+                                width={60}
+                            />
+                        </span>
+                    ))}
+                </div>
+            </div>
+
+            <div className="mt-5">
+                <h2 className="text-2xl font-semibold py-5 text-stone-800 dark:text-stone-300">
+                    Tools
+                </h2>
+
+                <div className="flex space-x-10 flex-wrap items-center flex-shrink-0">
+                    {tools.map((stack) => (
                         <span key={stack.alt}>
                             <Image
                                 className={`cursor-pointer transition-all duration-300 hover:border dark:border-[#0a0a0a] dark:hover:border-white p-2 border-stone-300 rounded-md ${
