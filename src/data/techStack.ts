c2959e157458c19166134dff23b79f2e9e94b2b9
Obasi Chizitere <blossomobasi2@gmail.com>
2024-08-19 11:22:33 +0100

    Add data for tools

--- src/data/techStack.ts
+++ src/data/techStack.ts
@@ -3,6 +3,7 @@ export const frontendStacks = [
     { src: "/images/css.svg", alt: "CSS" },
     { src: "/images/javascript.svg", alt: "Javascript" },
     { src: "/images/tailwindcss.svg", alt: "Tailwindcss" },
+    { src: "/images/figma.svg", alt: "Figma" },
     { src: "/images/react.svg", alt: "React" },
     { src: "/images/nextjs.svg", alt: "Nextjs", change: true },
     { src: "/images/typescript.svg", alt: "Typescript" },
@@ -13,4 +14,10 @@ export const backendStacks = [
     { src: "/images/nodejs.svg", alt: "Nodejs" },
     { src: "/images/supabase.png", alt: "Supabase" },
     { src: "/images/express.svg", alt: "Express js", change: true },
+];
+
+export const tools = [
+    { src: "/images/git.svg", alt: "Git", change: true },
+    { src: "/images/github.svg", alt: "Github", change: true },
+    { src: "/images/vscode.svg", alt: "Vscode" },
 ];
