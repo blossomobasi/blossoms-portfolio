e38d08b70dc673efc7a2914484b7a3a6dfd74f37
Obasi Chizitere <blossomobasi2@gmail.com>
2024-09-06 11:50:47 +0100

    Reduce gap

--- src/components/about/AboutSlice3.tsx
+++ src/components/about/AboutSlice3.tsx
@@ -2,15 +2,9 @@ import Image from "next/image";
 
 const AboutSlice3 = () => {
     return (
-        <div className="flex md:gap-x-20 gap-y-10 items-center md:my-28 my-16 flex-col md:flex-row">
+        <div className="flex md:gap-x-10 gap-y-10 items-center md:my-28 my-16 flex-col md:flex-row">
             <div className="w-full rounded-2xl overflow-hidden">
-                <Image
-                    src="/fun_moments.svg"
-                    alt="Laptop"
-                    className="h-[20rem] md:w-[30rem] w-full"
-                    width={1000}
-                    height={1000}
-                />
+                <Image src="/fun_moments.svg" alt="fun moments" width={1000} height={1000} />
             </div>
             <div className="w-full text-center md:text-left">
                 <h2 className="text-2xl font-semibold">
