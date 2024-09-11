ee21ec4c505e60322c0af23b5fdd92aa521bfbc1
Obasi Chizitere <blossomobasi2@gmail.com>
2024-09-11 09:02:30 +0100

    Remove unnecessary components and use directly in the about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,82 @@
-// import AboutSlice1 from "@/components/about/AboutSlice1";
-// import AboutSlice2 from "@/components/about/AboutSlice2";
-// import AboutSlice3 from "@/components/about/AboutSlice3";
-// import TextHeader from "@/components/TextHeader";
-// import Image from "next/image";
-
-import TextHeader from "@/components/TextHeader";
-import AboutSlice1 from "@/components/about/AboutSlice1";
-import AboutSlice2 from "@/components/about/AboutSlice2";
-import AboutSlice3 from "@/components/about/AboutSlice3";
 import Image from "next/image";
+import TextHeader from "@/components/TextHeader";
+
+const AboutSlice1 = () => {
+    return (
+        <div className="flex md:gap-x-10 gap-y-10 items-center flex-col md:flex-row">
+            <div className="h-fit w-full overflow-hidden">
+                <Image src="/dedicated_programmer.svg" alt="Laptop" width={1000} height={1000} />
+            </div>
+
+            <div className="w-full text-center md:text-left">
+                <h2 className="text-2xl font-semibold">
+                    A dedicated Developer based in Lagos, Nigeria 📍
+                </h2>
+                <p className="mt-3">
+                    I&apos;m currently pursuing a degree in Computer Science at Lagos State
+                    University, now in my final year. My journey in the world of technology has been
+                    both exciting and challenging.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+const AboutSlice2 = () => {
+    return (
+        <section className="flex items-center md:gap-x-10 gap-y-10 md:my-28 my-16 flex-col-reverse md:flex-row">
+            <div className="w-full text-center md:text-left">
+                <h2
+                    className="
+                text-2xl font-semibold"
+                >
+                    Collaborations ♻
+                </h2>
+                <p className="mt-3">
+                    I have worked with talented teams to create visually stunning websites for
+                    businesses and other exciting projects. I am a determined individual who prides
+                    myself on my tenacity when it comes to completing tasks.
+                </p>
+            </div>
+
+            <div className="w-full">
+                <Image
+                    className="w-full h-full"
+                    src="/collaboration.svg"
+                    alt="Team Collaboration"
+                    width={1000}
+                    height={1000}
+                />
+            </div>
+        </section>
+    );
+};
+
+const AboutSlice3 = () => {
+    return (
+        <div className="flex md:gap-x-10 gap-y-10 items-center md:my-28 my-16 flex-col md:flex-row">
+            <div className="w-full rounded-2xl overflow-hidden">
+                <Image src="/fun_moments.svg" alt="fun moments" width={1000} height={1000} />
+            </div>
+            <div className="w-full text-center md:text-left">
+                <h2 className="text-2xl font-semibold">
+                    What I do for fun beyond coding <span className="font-light">〰</span>
+                </h2>
+                <p className="mt-3">
+                    I find immense joy in problem-solving. While it may be challenging at times, the
+                    satisfaction of overcoming obstacles fuels my passion for programming.{" "}
+                    <span className="font-semibold text-black dark:text-stone-300">
+                        Beyond coding
+                    </span>
+                    , I have a love for diverse interests. I enjoy immersing myself in the world of
+                    gaming, particularly indulging in Call of Duty Mobile. Connecting with friends
+                    and family is another source of happiness for me. Oh, and I can&apos;t forget to
+                    mention my enthusiasm for anime &mdash; it&apos;s a delightful escape!
+                </p>
+            </div>
+        </div>
+    );
+};
 
 const AboutMe = () => {
     const softSkill = [
@@ -20,7 +88,7 @@ const AboutMe = () => {
         {
             title: "Adaptability",
             description:
-                "I thrive in dynamic environments, quickly adjusting to new challenges and opportunities. Whether it's learning new tools, embracing changes in project direction, or adapting to team needs, I remain flexible and open-minded.",
+                "I excel in dynamic environments, swiftly adjusting to new challenges and changes, and staying open-minded to evolving needs.",
         },
         {
             title: "Time Management",
@@ -30,7 +98,7 @@ const AboutMe = () => {
         {
             title: "Problem Solving",
             description:
-                "I approach challenges with a strategic mindset, thoroughly analyzing issues and exploring creative solutions.",
+                "I approach challenges with a growth mindset, using each experience as an opportunity to improve and develop. I thoroughly analyze issues and explore innovative solutions to overcome obstacles effectively.",
         },
         {
             title: "Communication",
@@ -39,21 +107,21 @@ const AboutMe = () => {
         },
     ];
     return (
-        <div className="lg:w-[60%] text-stone-800 dark:text-stone-400">
-            <h1 className="sm:text-5xl text-4xl font-semibold mt-5">I&apos;m Blossom Obasi.</h1>
+        <div className="lg:w-[60%] text-stone-700 dark:text-stone-400">
+            <h1 className="sm:text-5xl text-4xl font-bold mt-5">I&apos;m Blossom Obasi.</h1>
             <div className="mt-6">
                 <p>
-                    A software engineer that specializes in building dynamic and responsive web
-                    applications. I work with technologies on the client side like Javascript,
-                    React, Next.js, and Vite (with TypeScript for configuration).
+                    A self-taught software engineer specializing in building dynamic and responsive
+                    web applications. I work with client-side technologies such as JavaScript,
+                    React, Next.js, and Vite, with TypeScript for configuration.
                 </p>
                 <br />
                 <p>
-                    My journey in software development includes creating reusable components,
-                    managing state efficiently, and ensuring seamless user experiences across
-                    various devices. I approach each project with a scrupulous attention to detail,
-                    tackling challenges and finding solutions that meet both user and business
-                    needs.
+                    My journey in software development is driven by a passion for creating
+                    innovative, user-centered solutions. I approach every project with meticulous
+                    attention to detail, overcoming challenges with thoughtful problem-solving to
+                    deliver seamless experiences that align with both user expectations and business
+                    objectives.
                 </p>
                 <br />
                 <br />
