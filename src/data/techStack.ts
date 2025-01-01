0cbb26336e67f6adc694f05f46f588c3f3fce2ab
Blossom Obasi <blossomobasi2@gmail.com>
2025-01-01 19:25:18 +0100

    Add Expo and Docker icons to tech stack; reorder existing icons for consistency

--- src/data/techStack.ts
+++ src/data/techStack.ts
@@ -1,25 +1,27 @@
 export const frontendStacks = [
-    { src: "/images/html5.svg", alt: "HTML" },
-    { src: "/images/css.svg", alt: "CSS" },
-    { src: "/images/javascript.svg", alt: "Javascript" },
-    { src: "/images/tailwindcss.svg", alt: "Tailwindcss" },
-    { src: "/images/figma.svg", alt: "Figma" },
-    { src: "/images/react.svg", alt: "React" },
-    { src: "/images/nextjs.svg", alt: "Nextjs", change: true },
-    { src: "/images/typescript.svg", alt: "Typescript" },
+	{ src: "/images/html5.svg", alt: "HTML" },
+	{ src: "/images/css.svg", alt: "CSS" },
+	{ src: "/images/javascript.svg", alt: "Javascript" },
+	{ src: "/images/tailwindcss.svg", alt: "Tailwindcss" },
+	{ src: "/images/nextjs.svg", alt: "Nextjs", change: true },
+	{ src: "/images/figma.svg", alt: "Figma" },
+	{ src: "/images/react.svg", alt: "React" },
+	{ src: "/images/typescript.svg", alt: "Typescript" },
+	{ src: "/images/expo.svg", alt: "Expo", change: true },
 ];
 
 export const backendStacks = [
-    { src: "/images/mongodb.svg", alt: "Mongodb" },
-    { src: "/images/nodejs.svg", alt: "Nodejs" },
-    { src: "/images/express.svg", alt: "Expressjs", change: true },
-    { src: "/images/nestjs.svg", alt: "Nestjs" },
-    { src: "/images/supabase.png", alt: "Supabase" },
+	{ src: "/images/mongodb.svg", alt: "Mongodb" },
+	{ src: "/images/nodejs.svg", alt: "Nodejs" },
+	{ src: "/images/express.svg", alt: "Expressjs", change: true },
+	{ src: "/images/nestjs.svg", alt: "Nestjs" },
+	{ src: "/images/supabase.png", alt: "Supabase" },
 ];
 
 export const tools = [
-    { src: "/images/git.svg", alt: "Git", change: true },
-    { src: "/images/github.svg", alt: "Github", change: true },
-    { src: "/images/vscode.svg", alt: "Vscode" },
-    { src: "/images/postman.svg", alt: "Postman" },
+	{ src: "/images/git.svg", alt: "Git", change: true },
+	{ src: "/images/vscode.svg", alt: "Vscode" },
+	{ src: "/images/github.svg", alt: "Github", change: true },
+	{ src: "/images/postman.svg", alt: "Postman" },
+	{ src: "/images/docker.svg", alt: "Docker", change: true },
 ];
