69766f809322be05fe03fe9b15f4264937f13f7c
Blossom Obasi <blossomobasi2@gmail.com>
2025-01-09 17:32:28 +0100

    Update layout metadata for improved clarity and SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ import NextButton from "@/components/NextButton";
 import PrevButton from "@/components/PrevButton";
 
 const montserrat_alternates = Montserrat_Alternates({
-    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-    subsets: ["latin"],
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	subsets: ["latin"],
 });
 
 // const exo_2 = Exo_2({
@@ -21,30 +21,32 @@ const montserrat_alternates = Montserrat_Alternates({
 // });
 
 export const metadata: Metadata = {
-    title: "Blossom Obasi | Full-stack Developer",
-    description: "About Blossom Obasi",
+	title: "Blossom Obasi | Software Engineer",
+	description:
+		"Showcasing the projects, skills, and achievements of Blossom Obasi, a software engineer specializing in dynamic and high-performance web applications.",
+	keywords: "Software Engineer, React, React Native, Next.js, Node.js, Express, Nest.js",
 };
 
 export default function RootLayout({
-    children,
+	children,
 }: Readonly<{
-    children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-    return (
-        <DarkModeProvider>
-            <html lang="en">
-                <body
-                    className={`${montserrat_alternates.className} dark:bg-[#0a0a0a] text-stone-700 dark:text-stone-400 selection:bg-stone-500 selection:text-white`}
-                >
-                    <NavBar />
-                    <section className="lg:px-24 md:px-14 px-5 pt-16">{children}</section>
-                    <Footer />
+	return (
+		<DarkModeProvider>
+			<html lang="en">
+				<body
+					className={`${montserrat_alternates.className} dark:bg-[#0a0a0a] text-stone-700 dark:text-stone-400 selection:bg-stone-500 selection:text-white`}
+				>
+					<NavBar />
+					<section className="lg:px-24 md:px-14 px-5 pt-16">{children}</section>
+					<Footer />
 
-                    <ScrollUpButton />
-                    <PrevButton />
-                    <NextButton />
-                </body>
-            </html>
-        </DarkModeProvider>
-    );
+					<ScrollUpButton />
+					<PrevButton />
+					<NextButton />
+				</body>
+			</html>
+		</DarkModeProvider>
+	);
 }
