b3d0f96fbcc58dad6d5a8d179163b3a9b4d0c66a
Blossom Obasi <blossomobasi2@gmail.com>
2025-01-21 08:04:16 +0100

    Add Slide component animations to TextHeader for enhanced visual transitions

--- src/components/TextHeader.tsx
+++ src/components/TextHeader.tsx
@@ -1,10 +1,15 @@
+import { Slide } from "../../animation/Slide";
+
 const TextHeader = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <div className="uppercase font-extrabold text-stone-600 dark:text-stone-300 relative w-fit mb-10 text-xl italic -z-10 text-center">
-            {children}
-            <div className="absolute h-0.5 w-full bg-stone-600 dark:bg-stone-300" />
-        </div>
-    );
+	return (
+		<Slide
+			towards="left"
+			className="uppercase font-extrabold text-stone-600 dark:text-stone-300 relative w-fit mb-10 text-xl italic -z-10 text-center"
+		>
+			{children}
+			<div className="absolute h-0.5 w-full bg-stone-600 dark:bg-stone-300" />
+		</Slide>
+	);
 };
 
 export default TextHeader;
